67068efa5fef26f46836d4c714beebc2241a5859
Ben Burwell <bburwell1@gmail.com>
2013-05-07 19:28:31 -0400

    Adds V1 of Safari extension

---
+++ Safari/Helvetica.safariextension/helvetica.js
@@ -0,0 +1,10 @@
+/**
+ * Helvetica.js
+ * Makes stuff Helvetica
+ */
+
+var elms[] = document.all;
+
+for (var i = 0; i < elms.length; i++) {
+	elms[i].style.fontFamily="'Helvetica Neue','Helvetica'";
+}
